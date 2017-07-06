953204ffac947d690dca03431f39a8047a462ac1
hojin <hojin@cstone.co.kr>
2017-07-06 10:23:05 +0900

    hojin

--- public/src/pjtCom/js/myteacher.js
+++ public/src/pjtCom/js/myteacher.js
@@ -10,7 +10,7 @@ myTeacher([
 	},
 
 	{
-		path : '/sub',
+		path : '/learning',
 		controller : 'sub'
 	}
 ],{
@@ -19,6 +19,35 @@ myTeacher([
 	}, // common
 
     sub : function(){
+        $(window).load(function(){
+			var drag = false;
 
+            $('.answer_list li').mousedown(function(){
+				drag = true;
+				var offset = {
+					top : $(this).offset().top,
+					left : $(this).offset().left
+				};
+
+				$(this).addClass('click');
+				var answerCopy = $(this).clone();
+
+				$('.answer_sheet').append(answerCopy);
+			});
+
+			$('.answer_area').mouseup(function(){
+				drag = false;
+				$('.answer_sheet li').removeClass('click');
+			}).mousemove(function(e){
+				if (drag) {
+					var mouse = {
+						x : e.pageX,
+						y : e.pageY
+					};
+
+					$('.answer_area li.click').css({'top':+mouse.y, 'left':+mouse.x});
+				}
+			});
+        });
 	} // sub
 });
